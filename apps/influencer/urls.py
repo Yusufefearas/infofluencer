e05f3b63c203ff2dbce6a735144db582067826ea
Yusuf Hamza Kıran <143087222+yusufhkran@users.noreply.github.com>
2025-06-27 12:23:00 +0300

    influencer dashboard, login ve register güncellemeleri.

--- apps/influencer/urls.py
+++ apps/influencer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard/",views.influencer_dashboard,name="influencer_dashboard")    
+    path("dashboard/",views.influencer_dashboard,name="influencer_dashboard"),
+    path("logout/",views.influencer_logout,name="influencer_logout"),
 ]
--- apps/influencer/views.py
+++ apps/influencer/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout,login
 
 def influencer_dashboard(request):
     return render(request,"influencer/dashboard.html")
+def influencer_logout(request):
+    logout(request)
+    return redirect("home")
